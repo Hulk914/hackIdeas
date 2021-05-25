f95a6312e1276f8700cad61633ff4a7fec8c3b51
Hulk914 <ayushthakur2009@gmail.com>
2021-05-25 05:42:27 +0530

    initial setup and login design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((mod) => mod.LoginModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
